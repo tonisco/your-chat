4c62680d76e8c97994d9cd55e9abb3a6376b24dd
tonisco <tonyumeozulu@gmail.com>
2023-11-08 09:49:19 +0100

    cors next

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,29 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_API_URL,
+          }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
