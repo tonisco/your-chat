acd8a0a54b625c26736ca4e6a417f4506e97abe9
tonisco <tonyumeozulu@gmail.com>
2023-11-09 20:20:50 +0100

    next config single build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,29 +4,7 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: process.env.NEXT_PUBLIC_API_URL,
-          }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ]
-  },
+  output: "standalone",
 }
 
 module.exports = nextConfig
